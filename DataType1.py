c29e01f59c6bfbbe0e85f65f0e773b01aaf64ee4
OM ULHAS BURSE <omburse0019@gmail.com>
2025-06-06 01:51:34 +0530

    Add files via upload

---
+++ DataType1.py
@@ -0,0 +1,12 @@
+a = 10 # 'a' is a Integer value
+b = 10.87 # b is a Floating point number
+c = "Om" # c is a String
+d = True # d is a Boolean
+e = None # It is a Empty value
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
---
+++ variable1.py
@@ -0,0 +1,8 @@
+#  x = 5
+#  X = "Om"
+#  print(type(x))
+#  print(type(X))
+
+x = 5
+x = 10
+print(x)
