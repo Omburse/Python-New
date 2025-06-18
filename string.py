587c9fdbb62cf6585ae6b5dee06799d5da890870
OM ULHAS BURSE <omburse0019@gmail.com>
2025-06-19 01:01:31 +0530

    String in Python

---
+++ string.py
@@ -0,0 +1,9 @@
+OM = "Omburse"
+print(OM[-3:-2])
+a = "Burse"
+print(a.replace("B","P"))
+str = " OM OM OM  OM OM OM OM "
+print(str.count("OM"))
+
+om = "The Strip method are used for removes a whitespace"
+print(om.endswith(" whitespace"))
